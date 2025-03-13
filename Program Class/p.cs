a4daf4e538837c86def063387abcb05d0de14892
kingsley2-design <brightnya54@gmail.com>
2025-03-13 15:06:44 +0100

    my work

---
+++ Program Class/p.cs
@@ -0,0 +1,46 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Journal journal = new Journal();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("\nJournal Menu:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display journal");
+            Console.WriteLine("3. Save journal to a file");
+            Console.WriteLine("4. Load journal from a file");
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option: ");
+
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    journal.AddEntry();
+                    break;
+                case "2":
+                    journal.DisplayJournal();
+                    break;
+                case "3":
+                    Console.Write("Enter filename: ");
+                    journal.SaveToFile(Console.ReadLine());
+                    break;
+                case "4":
+                    Console.Write("Enter filename: ");
+                    journal.LoadFromFile(Console.ReadLine());
+                    break;
+                case "5":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice, please try again.");
+                    break;
+            }
+        }
+    }
+}
